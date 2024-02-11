6c5a717baeab3ea4ce7a9c08e17be67c38638a52
skorpion <jmosinski@o2.pl>
2024-02-11 18:56:06 +0100

    test

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './Home.module.css'
 
 const Home = () => {
+	const [count, setCount] = useState({
+		count: 5,
+	})
+
+	const interval = setInterval(() => {
+		setCount({
+			count: count.count - 1,
+		})
+		if (count.count <= 0) {
+			setCount({
+				count: null,
+			})
+			clearInterval(interval)
+		}
+	}, 1000)
+
 	return (
 		<div className={styles.wrapper_home}>
-			<h1>HOME</h1>
+			<div className={styles.boxHome_heading}>
+				<h1 className={styles.home_heading}>Component Home</h1>
+				<h4 className={styles.home_theme}>moje wariacje z przywitaniem aplikacji</h4>
+			</div>
+			<div className={styles.boxHome_action}>
+				<h1 className={styles.home_count}>{count.count}</h1>
+			</div>
 		</div>
 	)
 }
