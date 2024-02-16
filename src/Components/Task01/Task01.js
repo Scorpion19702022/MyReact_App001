4544f3e8c6775b798d8999fd54c57627a4f3b7c5
skorpion <jmosinski@o2.pl>
2024-02-16 21:03:00 +0100

    test

--- src/Components/Task01/Task01.js
+++ src/Components/Task01/Task01.js
@@ -1,16 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './Task01.module.css'
 
 const Task01 = () => {
 	return (
 		<div className={styles.wrapper_task01}>
 			<h1>Task01</h1>
-			<div className={styles.box}>
-				<div className={styles.in}>
-					<input type='text' value={info} onChange={handleChange} />
-					<button className={styles.btn}>Kliknij</button>
-				</div>
-			</div>
 		</div>
 	)
 }
