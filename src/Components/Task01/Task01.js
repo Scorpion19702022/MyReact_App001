57814ad4b69860e0547051d23ad176a5faf3a9e6
skorpion <jmosinski@o2.pl>
2024-02-16 21:02:06 +0100

    test

--- src/Components/Task01/Task01.js
+++ src/Components/Task01/Task01.js
@@ -2,12 +2,6 @@ import React, { useState } from 'react'
 import styles from './Task01.module.css'
 
 const Task01 = () => {
-	const [info, setInfo] = useState('')
-
-	const handleChange = e => {
-		setInfo(e.target.value)
-	}
-
 	return (
 		<div className={styles.wrapper_task01}>
 			<h1>Task01</h1>
@@ -16,7 +10,6 @@ const Task01 = () => {
 					<input type='text' value={info} onChange={handleChange} />
 					<button className={styles.btn}>Kliknij</button>
 				</div>
-				<h4>Tekst po kliknięciu: {info}</h4>
 			</div>
 		</div>
 	)
