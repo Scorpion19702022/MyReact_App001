ddf1f449d2d23896fa43029ded78599bea4e493d
skorpion <jmosinski@o2.pl>
2024-02-16 21:01:05 +0100

    test

--- src/Components/Task01/Task01.js
+++ src/Components/Task01/Task01.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Task01.module.css'
 
 const Task01 = () => {
+	const [info, setInfo] = useState('')
+
+	const handleChange = e => {
+		setInfo(e.target.value)
+	}
+
 	return (
 		<div className={styles.wrapper_task01}>
 			<h1>Task01</h1>
+			<div className={styles.box}>
+				<div className={styles.in}>
+					<input type='text' value={info} onChange={handleChange} />
+					<button className={styles.btn}>Kliknij</button>
+				</div>
+				<h4>Tekst po kliknięciu: {info}</h4>
+			</div>
 		</div>
 	)
 }
