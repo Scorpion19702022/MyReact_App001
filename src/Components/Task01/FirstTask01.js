0deca0a24f1c4e8ed8403ff3ad4d4b6be35d03d8
skorpion <jmosinski@o2.pl>
2024-02-17 23:29:02 +0100

    test

--- src/Components/Task01/FirstTask01.js
+++ src/Components/Task01/FirstTask01.js
@@ -13,13 +13,17 @@ const FirstTask01 = () => {
 					<h2 className={styles.heading_task01_a1}>Wyloguj literę</h2>
 					<div className={styles.inputs_task01_a1}>
 						<input className={styles.input_task01_a1} type='text' value={textTask01a} onChange={handleChange} />
-						<button className={styles.btn_task01_a1} onClick={handleClickAddTextInTask01a}>
+						<button
+							className={!changeState ? styles.btn_task01_a1 : styles.btn_task01_a1_sub}
+							onClick={handleClickAddTextInTask01a}
+						>
 							{!changeState ? 'zobacz' : 'usuń'}
 						</button>
 					</div>
 					<h3 className={styles.result_task01_a1}>
 						Twój wpis: <span className={styles.result_task01_a1_span}>{textView}</span>
 					</h3>
+					{textView !== '' ? <p className={styles.task01_success}>zadanie zakończoe sukcesem</p> : null}
 				</div>
 			</div>
 		</div>
