d84f4de1f6f12a17595e87719879a609325c02a9
skorpion <jmosinski@o2.pl>
2024-02-17 22:30:57 +0100

    test context

---
+++ src/Components/Task01/FirstTask01.js
@@ -0,0 +1,23 @@
+import styles from './Task01.module.css'
+
+const FirstTask01 = () => {
+	return (
+		<div className={styles.wrapper_task01}>
+			<h1 className={styles.heading_page}>Task01</h1>
+			<div className={styles.box_task01_a}>
+				<div className={styles.box_task01_a1}>
+					<h2 className={styles.heading_task01_a1}>Wyloguj literę</h2>
+					<div className={styles.inputs_task01_a1}>
+						<input className={styles.input_task01_a1} type='text' />
+						<button className={styles.btn_task01_a1}>Kliknij</button>
+					</div>
+					<h3 className={styles.result_task01_a1}>
+						Twój wpis: <span className={styles.result_task01_a1_span}></span>
+					</h3>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default FirstTask01
--- src/Components/Task01/Task01.js
+++ src/Components/Task01/Task01.js
@@ -1,31 +1,8 @@
-import React, { useContext } from 'react'
-import styles from './Task01.module.css'
-import Task01Context, { AddFunctionsTask01Provider } from './Context/Task01Context'
+import React from 'react'
+import FirstTask01 from './FirstTask01'
 
 const Task01 = () => {
-	const handleClickAddTextInTask01a = useContext(Task01Context)
-
-	return (
-		<div className={styles.wrapper_task01}>
-			<h1 className={styles.heading_page}>Task01</h1>
-			<AddFunctionsTask01Provider>
-				<div className={styles.box_task01_a}>
-					<div className={styles.box_task01_a1}>
-						<h2 className={styles.heading_task01_a1}>Wyloguj literę</h2>
-						<div className={styles.inputs_task01_a1}>
-							<input className={styles.input_task01_a1} type='text' />
-							<button className={styles.btn_task01_a1} onClick={handleClickAddTextInTask01a}>
-								Kliknij
-							</button>
-						</div>
-						<h3 className={styles.result_task01_a1}>
-							Twój wpis: <span className={styles.result_task01_a1_span}></span>
-						</h3>
-					</div>
-				</div>
-			</AddFunctionsTask01Provider>
-		</div>
-	)
+	return <FirstTask01 />
 }
 
 export default Task01
