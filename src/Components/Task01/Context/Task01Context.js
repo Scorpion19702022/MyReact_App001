a28e5c852bcde791d6a0392ef28769cc34331cdf
skorpion <jmosinski@o2.pl>
2024-02-17 19:27:14 +0100

    tezt context first

---
+++ src/Components/Task01/Context/Task01Context.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from 'react'
+
+const Task01Context = createContext()
+
+export const AddFunctionsTask01Provider = ({ children }) => {
+	const [textTask01a, setTextTask01a] = useState('')
+
+	const [changeState, setChangeState] = useState(false)
+
+	const handleClickAddTextInTask01a = e => {
+		setTextTask01a(e.target.value)
+		setTextTask01a('')
+		setChangeState(!changeState)
+	}
+
+	return (
+		<Task01Context.Provider value={{ textTask01a, changeState, handleClickAddTextInTask01a }}>
+			{children}
+		</Task01Context.Provider>
+	)
+}
+
+export default Task01Context
