930a78be0f3f6b3ce4661ef764c05c598c043555
skorpion <jmosinski@o2.pl>
2024-02-17 22:37:22 +0100

    success test context

--- src/Components/Task01/FirstTask01.js
+++ src/Components/Task01/FirstTask01.js
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
 import styles from './Task01.module.css'
+import Task01Context from './Context/Task01Context'
 
 const FirstTask01 = () => {
+	const { handleClickAddTextInTask01a, textTask01a, changeState } = useContext(Task01Context)
+
 	return (
 		<div className={styles.wrapper_task01}>
 			<h1 className={styles.heading_page}>Task01</h1>
@@ -9,7 +13,9 @@ const FirstTask01 = () => {
 					<h2 className={styles.heading_task01_a1}>Wyloguj literę</h2>
 					<div className={styles.inputs_task01_a1}>
 						<input className={styles.input_task01_a1} type='text' />
-						<button className={styles.btn_task01_a1}>Kliknij</button>
+						<button className={styles.btn_task01_a1} onClick={handleClickAddTextInTask01a}>
+							Kliknij
+						</button>
 					</div>
 					<h3 className={styles.result_task01_a1}>
 						Twój wpis: <span className={styles.result_task01_a1_span}></span>
--- src/Components/Task01/Task01.js
+++ src/Components/Task01/Task01.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import FirstTask01 from './FirstTask01'
+import { AddFunctionsTask01Provider } from './Context/Task01Context'
 
 const Task01 = () => {
-	return <FirstTask01 />
+	return (
+		<AddFunctionsTask01Provider>
+			<FirstTask01 />
+		</AddFunctionsTask01Provider>
+	)
 }
 
 export default Task01
