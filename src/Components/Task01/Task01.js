6f3d0748fdce600aa144e0e89c12925733d5ffe9
skorpion <jmosinski@o2.pl>
2024-02-17 18:24:09 +0100

    added all styles for component Task01

--- src/Components/Task01/Task01.js
+++ src/Components/Task01/Task01.js
@@ -10,9 +10,11 @@ const Task01 = () => {
 					<h2 className={styles.heading_task01_a1}>Wyloguj literę</h2>
 					<div className={styles.inputs_task01_a1}>
 						<input className={styles.input_task01_a1} type='text' />
-						<button className={styles.btn_task01_a1}></button>
+						<button className={styles.btn_task01_a1}>Kliknij</button>
 					</div>
-					<h3 className={styles.result_task01_a1}>Twój wpis:</h3>
+					<h3 className={styles.result_task01_a1}>
+						Twój wpis: <span className={styles.result_task01_a1_span}></span>
+					</h3>
 				</div>
 			</div>
 		</div>
