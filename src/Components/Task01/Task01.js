387d4529795b64177515900e1287de8048fe0d3b
skorpion <jmosinski@o2.pl>
2024-02-17 17:23:06 +0100

    prepared first task in component Task01

--- src/Components/Task01/Task01.js
+++ src/Components/Task01/Task01.js
@@ -4,7 +4,17 @@ import styles from './Task01.module.css'
 const Task01 = () => {
 	return (
 		<div className={styles.wrapper_task01}>
-			<h1>Task01</h1>
+			<h1 className={styles.heading_page}>Task01</h1>
+			<div className={styles.box_task01_a}>
+				<div className={styles.box_task01_a1}>
+					<h2 className={styles.heading_task01_a1}>Wyloguj literę</h2>
+					<div className={styles.inputs_task01_a1}>
+						<input type='text' />
+						<button className={styles.btn_task01_a1}></button>
+					</div>
+					<h3 className={styles.result_task01_a1}>Twój wpis:</h3>
+				</div>
+			</div>
 		</div>
 	)
 }
