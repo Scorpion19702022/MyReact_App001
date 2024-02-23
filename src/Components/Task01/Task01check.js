d7396acece79d454390a82b5a31c042b814d72e7
skorpion <jmosinski@o2.pl>
2024-02-23 22:00:42 +0100

    prepared structure

--- src/Components/Task01/Task01check.js
+++ src/Components/Task01/Task01check.js
@@ -6,6 +6,24 @@ const Task01check = () => {
 	return (
 		<div className={styles.wrapper_checkbox}>
 			<h1 className={styles.heading_check}>Test z zastosowaiem checkboxa</h1>
+			<div className={styles.box_check}>
+				<div className={styles.label_input}>
+					<label className={styles.label_check}>podaj swoje imię</label>
+					<input className={styles.input_check} type='text' />
+					<p className={styles.check_error}></p>
+				</div>
+				<div className={styles.label_checks}>
+					<label className={styles.label_check}>Masz 18 lat</label>
+					<input className={styles.input_check_alone} type='checkbox' />
+				</div>
+				<div className={styles.btns_check}>
+					<button className={styles.btn_check}>sprawdź</button>
+				</div>
+			</div>
+			<div className={styles.check_results}>
+				<h2 className={styles.check_result}></h2>
+			</div>
+			<p className={styles.task_check_success}></p>
 		</div>
 	)
 }
