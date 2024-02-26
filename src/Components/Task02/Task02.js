7969a07fc99535396b5aff9834bdda0bb1c1f72b
skorpion <jmosinski@o2.pl>
2024-02-26 22:39:26 +0100

    test

--- src/Components/Task02/Task02.js
+++ src/Components/Task02/Task02.js
@@ -3,11 +3,16 @@ import React from 'react'
 import styles from './Task02.module.css'
 import T02list from './T02list'
 
+import WomanMan from './assets/woman_man.png'
+
 const Task02 = () => {
 	return (
 		<div className={styles.wrapper_task02}>
 			<h1 className={styles.heading_task02}>Task 02</h1>
 			<div className={styles.wrapper_task02_main}>
+				<div className={styles.box_images}>
+					<img src={WomanMan} alt='kobieta i mężczyzna' />
+				</div>
 				<T02list />
 			</div>
 		</div>
