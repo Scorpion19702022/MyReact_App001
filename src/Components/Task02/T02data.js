0c72efc2a571a8d6431052035abaa4a809ccdff2
skorpion <jmosinski@o2.pl>
2024-02-26 21:54:32 +0100

    test

--- src/Components/Task02/T02data.js
+++ src/Components/Task02/T02data.js
@@ -1,11 +1,34 @@
-export const People = [
-	{
-		name: 'Beata',
-		age: 51,
-		nation: 'Polska',
-		city: 'Łódź',
-		job: 'nauczycielka',
-		img: '',
-		sex: 'kobieta',
-	},
-]
+import React from 'react'
+
+const T02data = () => {
+	const people = [
+		{
+			name: 'Beata',
+			age: 52,
+			nation: 'Polska',
+			city: 'Łódź',
+			job: 'nauczycielka',
+			img: '',
+			sex: 'kobieta',
+		},
+		{
+			name: 'Jacek',
+			age: 53,
+			nation: 'Polska',
+			city: 'Łódź',
+			job: 'grafik/programista',
+			img: '',
+			sex: 'mężczyzna',
+		},
+	]
+
+	const peopleList = people.map((item, index) => (
+		<div key={index}>
+			<h2>{item.name}</h2>
+		</div>
+	))
+
+	return <div>{peopleList}</div>
+}
+
+export default T02data
--- src/Components/Task02/T02list.js
+++ src/Components/Task02/T02list.js
@@ -1,7 +1,7 @@
 import T02data from './T02data'
 import styles from './T02list.module.css'
 
-const T02list = ({ peopleList }) => {
+const T02list = () => {
 	return (
 		<div className={styles.wrapper_list}>
 			<div>
