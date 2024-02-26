d654b5d2e0372f153ebc143d33842ec7cf4ccb61
skorpion <jmosinski@o2.pl>
2024-02-26 21:21:21 +0100

    start task02

--- src/Components/Task02/Task02.js
+++ src/Components/Task02/Task02.js
@@ -5,7 +5,7 @@ import styles from './Task02.module.css'
 const Task02 = () => {
 	return (
 		<div className={styles.wrapper_task02}>
-			<h1>Task 02</h1>
+			<h1 className={styles.heading_task02}>Task 02</h1>
 		</div>
 	)
 }
