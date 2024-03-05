a2d9d9f63d5bfb92dfb75ada990457390672e8ce
skorpion <jmosinski@o2.pl>
2024-03-05 19:29:41 +0100

    start Task03

--- src/Components/Task03/Task03.js
+++ src/Components/Task03/Task03.js
@@ -4,9 +4,10 @@ import styles from './Task03.module.css'
 
 const Task03 = () => {
 	return (
-		<div className={styles.wrapper_task02}>
+		<div className={styles.wrapper_task03}>
 			<div className={styles.task03_header}>
-				<h1>Task03</h1>
+				<h1 className={styles.task03_heading}>Task03</h1>
+				<p className={styles.task03_theme}>Kalkulator BMI z wykorzystaniem CONTEXT</p>
 			</div>
 		</div>
 	)
