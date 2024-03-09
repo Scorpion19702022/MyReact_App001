f523784d4b50b5b94d5e8dc0dc1f0b666e12bf39
skorpion <jmosinski@o2.pl>
2024-03-09 20:29:46 +0100

    added styles for compoet Task04result

--- src/Components/Task04/Task04result.js
+++ src/Components/Task04/Task04result.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './Task04result.module.css'
+import SalaryContext from './Context/SalaryContext'
 
 const Task04result = () => {
+	const { error } = useContext(SalaryContext)
+
 	return (
 		<div className={styles.wrapper_result}>
 			<div className={styles.box_result_salary}>
-				<h1 className={styles.heading_salary}>
-					Twoje wynagrodzenie: <span className={styles.heading_salary_result}></span>
-				</h1>
+				<h3 className={styles.heading_salary}>Twoje wynagrodzenie netto:</h3>
+				<h1 className={styles.result_salary}>000 zł</h1>
+				<p className={styles.salary_errors}>{error}</p>
 			</div>
 			<div className={styles.box_all_info}>
 				<h3 className={styles.heading_info}>Składki:</h3>
