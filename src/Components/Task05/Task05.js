d01ffef03478812a3605d2064caf2a11e48bcc40
skorpion <jmosinski@o2.pl>
2024-03-11 21:31:16 +0100

    test

--- src/Components/Task05/Task05.js
+++ src/Components/Task05/Task05.js
@@ -9,6 +9,9 @@ const Task05 = () => {
 				<h1 className={styles.heading_task05}>Task 05</h1>
 				<h4 className={styles.theme_task05}>Test z inputami</h4>
 			</div>
+			<div className={styles.box_tasks}>
+				<div className={styles.task_01}></div>
+			</div>
 		</div>
 	)
 }
