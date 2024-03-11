48fd7298e3656dc746a260101cd5722b9d391976
skorpion <jmosinski@o2.pl>
2024-03-11 21:47:27 +0100

    test

--- src/Components/Task05/Task05task1.js
+++ src/Components/Task05/Task05task1.js
@@ -3,7 +3,25 @@ import React from 'react'
 import styles from './Task05task1.module.css'
 
 const Task05task1 = () => {
-	return <div className={styles.task_01}></div>
+	return (
+		<div className={styles.task_01}>
+			<h2 className={styles.task01_heading}>Oblicz</h2>
+			<div className={styles.task01_box_inputs}>
+				<label className={styles.task01_label}></label>
+				<input className={styles.task01_input} type='text' />
+				<label className={styles.task01_label}></label>
+				<input className={styles.task01_input} type='text' />
+				<label className={styles.task01_label}></label>
+				<select name='' id=''>
+					<option value='0'>Miasto</option>
+					<option value=''>Łódź</option>
+					<option value=''>Warszawa</option>
+					<option value=''>Gdańsk</option>
+					<option value=''>Kraków</option>
+				</select>
+			</div>
+		</div>
+	)
 }
 
 export default Task05task1
