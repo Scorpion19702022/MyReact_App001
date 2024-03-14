248f0f791b51c6548a6faedfe4fc6ace98e5372d
skorpion <jmosinski@o2.pl>
2024-03-14 20:39:35 +0100

    test

--- src/Components/Task05/Context/Task05Context02.js
+++ src/Components/Task05/Context/Task05Context02.js
@@ -1,9 +1,12 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 const Task05Context02 = createContext()
 
 export const ActionTaskContext02 = ({ children }) => {
-	return <Task05Context02.Provider>{children}</Task05Context02.Provider>
+	const [celsius, setCelsius] = useState('℃')
+	const [fare, setFare] = useState('℉')
+
+	return <Task05Context02.Provider value={{ celsius, fare }}>{children}</Task05Context02.Provider>
 }
 
 export default Task05Context02
--- src/Components/Task05/Task05task2.js
+++ src/Components/Task05/Task05task2.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './Task05task2.module.css'
+import Task05Context02 from './Context/Task05Context02'
 
 const Task05task2 = () => {
+	const { celsius, fare } = useContext(Task05Context02)
+
 	return (
 		<div className={styles.wrapper_task2}>
 			<h2 className={styles.heading_task2}>Sprawdź ile stopni ℃ to stopni ℉ i odwrotnie</h2>
 			<div className={styles.box_into}>
-				<label className={styles.label}>Konwertuj z na</label>
+				<label className={styles.label}>
+					Konwertuj z {celsius} na {fare}
+				</label>
 				<input className={styles.input} type='number' />
 			</div>
 			<div className={styles.box_btns}>
