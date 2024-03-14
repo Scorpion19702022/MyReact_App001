667e5822dfa73f6dcd22a8a9205107e8cc9dba72
skorpion <jmosinski@o2.pl>
2024-03-14 20:53:47 +0100

    test

--- src/Components/Task05/Context/Task05Context02.js
+++ src/Components/Task05/Context/Task05Context02.js
@@ -6,7 +6,30 @@ export const ActionTaskContext02 = ({ children }) => {
 	const [celsius, setCelsius] = useState('℃')
 	const [fare, setFare] = useState('℉')
 
-	return <Task05Context02.Provider value={{ celsius, fare }}>{children}</Task05Context02.Provider>
+	const [stateButton, setStateButton] = useState(false)
+
+	const [intoInput, setIntoInput] = useState('')
+
+	const handleChangeInput = e => {
+		setIntoInput(e.target.value)
+	}
+
+	const handleClickChange = () => {
+		setStateButton(!stateButton)
+		if (!stateButton) {
+			setCelsius('℉')
+			setFare('℃')
+		} else {
+			setCelsius('℃')
+			setFare('℉')
+		}
+	}
+
+	return (
+		<Task05Context02.Provider value={{ celsius, fare, intoInput, handleChangeInput, handleClickChange }}>
+			{children}
+		</Task05Context02.Provider>
+	)
 }
 
 export default Task05Context02
--- src/Components/Task05/Task05task2.js
+++ src/Components/Task05/Task05task2.js
@@ -4,7 +4,7 @@ import styles from './Task05task2.module.css'
 import Task05Context02 from './Context/Task05Context02'
 
 const Task05task2 = () => {
-	const { celsius, fare } = useContext(Task05Context02)
+	const { celsius, fare, intoInput, handleChangeInput, handleClickChange } = useContext(Task05Context02)
 
 	return (
 		<div className={styles.wrapper_task2}>
@@ -13,12 +13,14 @@ const Task05task2 = () => {
 				<label className={styles.label}>
 					Konwertuj z {celsius} na {fare}
 				</label>
-				<input className={styles.input} type='number' />
+				<input className={styles.input} type='number' value={intoInput} onChange={handleChangeInput} />
 			</div>
 			<div className={styles.box_btns}>
 				<button className={styles.btn}>Konwertuj</button>
 				<button className={styles.btn}>Reset</button>
-				<button className={styles.btn}>Zmień</button>
+				<button className={styles.btn} onClick={handleClickChange}>
+					Zmień
+				</button>
 			</div>
 			<div className={styles.box_info}>
 				<h1 className={styles.heading_info}>Wynik:</h1>
