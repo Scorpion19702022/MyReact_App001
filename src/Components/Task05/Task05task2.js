3067208ff3abe826814b39fd1b3c920419fb0a95
skorpion <jmosinski@o2.pl>
2024-03-14 19:45:15 +0100

    prepared structure

--- src/Components/Task05/Task05task2.js
+++ src/Components/Task05/Task05task2.js
@@ -6,6 +6,19 @@ const Task05task2 = () => {
 	return (
 		<div className={styles.wrapper_task2}>
 			<h2 className={styles.heading_task2}>Sprawdź ile stopni ℃ to stopni ℉ i odwrotnie</h2>
+			<div className={styles.box_into}>
+				<label className={styles.label}>Konwertuj z na</label>
+				<input className={styles.input} type='number' />
+			</div>
+			<div className={styles.box_btns}>
+				<button className={styles.btn}>Konwertuj</button>
+				<button className={styles.btn}>Reset</button>
+				<button className={styles.btn}>Zmień</button>
+			</div>
+			<div className={styles.box_info}>
+				<h1 className={styles.heading_info}>Wynik:</h1>
+				<p className={styles.info}></p>
+			</div>
 		</div>
 	)
 }
