9b77aa7fce0da3e100c12dde08e069c168026281
skorpion <jmosinski@o2.pl>
2024-03-23 18:56:51 +0100

    test

--- src/Components/Task06/Task06.js
+++ src/Components/Task06/Task06.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 import styles from './Task06.module.css'
+import Task06country from './Task06country'
 
 const Task06 = () => {
 	return (
 		<div className={styles.wrapper_task06}>
 			<div className={styles.box_header}>
-				<h1 className={styles.header_heading}>Task06</h1>
+				<h1 className={styles.header_heading}>Task 06</h1>
 				<h4 className={styles.header_theme}>Zabawa z select</h4>
 			</div>
+			<Task06country />
 		</div>
 	)
 }
---
+++ src/Components/Task06/Task06country.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Task06country.module.css'
+
+const Task06country = () => {
+	return (
+		<div className={styles.wrapper_country}>
+			<h1 className={styles.heading_country}>Województwa w Polsce. Wybierz i przeczytaj opis</h1>
+		</div>
+	)
+}
+
+export default Task06country
