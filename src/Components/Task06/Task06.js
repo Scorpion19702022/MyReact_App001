d1b4c6ca02e645a182ad892893f03a679d507d3b
skorpion <jmosinski@o2.pl>
2024-03-23 23:06:44 +0100

    prepared context

--- src/Components/Task06/Task06.js
+++ src/Components/Task06/Task06.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import styles from './Task06.module.css'
 import Task06country from './Task06country'
+import ChhoseContext from './Context/ActionCountryContext'
 
 const Task06 = () => {
 	return (
@@ -10,7 +11,9 @@ const Task06 = () => {
 				<h1 className={styles.header_heading}>Task 06</h1>
 				<h4 className={styles.header_theme}>Zabawa z select</h4>
 			</div>
-			<Task06country />
+			<ChhoseContext>
+				<Task06country />
+			</ChhoseContext>
 		</div>
 	)
 }
