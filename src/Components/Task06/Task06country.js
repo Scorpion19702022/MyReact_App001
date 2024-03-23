ffa9f262955bfa0f5654e68cadac6412b90b146a
skorpion <jmosinski@o2.pl>
2024-03-23 20:56:24 +0100

    test

--- src/Components/Task06/Task06country.js
+++ src/Components/Task06/Task06country.js
@@ -32,7 +32,7 @@ const Task06country = () => {
 	return (
 		<div className={styles.wrapper_country}>
 			<h1 className={styles.heading_country}>Województwa w Polsce. Wybierz i przeczytaj opis</h1>
-			<div className={styles.wrapper_contet}>
+			<div className={styles.wrapper_content}>
 				<div className={styles.box_iputs}>
 					<div className={styles.box_input}>
 						<label className={styles.label}>Wybierz województwo:</label>
