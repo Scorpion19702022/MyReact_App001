137543fc6dd0c2d4f284ed8d06d663cad9c35fde
skorpion <jmosinski@o2.pl>
2024-03-26 20:12:37 +0100

    test

--- src/Components/Task07/Task07.js
+++ src/Components/Task07/Task07.js
@@ -9,6 +9,7 @@ const Task07 = () => {
 		<div className={styles.wrapper_task07}>
 			<div className={styles.box_header}>
 				<h1 className={styles.heading_header}>Coffee & Snack</h1>
+				<div className={styles.text_shadow}></div>
 				<div className={styles.heading_line}></div>
 				<div className={styles.img_header}>
 					<img className={styles.img_bar} src={Bar} alt='zdjęcie baru' />
