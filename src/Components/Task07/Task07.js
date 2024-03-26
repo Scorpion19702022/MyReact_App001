02c2675db5c65b37390f1bc3340805a0c9f88c65
skorpion <jmosinski@o2.pl>
2024-03-26 18:49:28 +0100

    start Task07

---
+++ src/Components/Task07/Task07.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Task07.module.css'
+
+const Task07 = () => {
+	return (
+		<div className={styles.wrapper_task07}>
+			<div className={styles.box_header}></div>
+		</div>
+	)
+}
+
+export default Task07
