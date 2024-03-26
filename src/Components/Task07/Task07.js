c515161f268fce732eff9e278f0945dfbb4313e8
skorpion <jmosinski@o2.pl>
2024-03-26 19:14:20 +0100

    test

--- src/Components/Task07/Task07.js
+++ src/Components/Task07/Task07.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Bar from './assets/bar.jpg'
+
 import styles from './Task07.module.css'
 
 const Task07 = () => {
@@ -7,7 +9,10 @@ const Task07 = () => {
 		<div className={styles.wrapper_task07}>
 			<div className={styles.box_header}>
 				<h1 className={styles.heading_header}>Coffee & Snack</h1>
-				<div></div>
+				<div className={styles.img_header}>
+					<img className={styles.img_bar} src={Bar} alt='zdjęcie baru' />
+				</div>
+				<div className={styles.shadow_header}></div>
 			</div>
 		</div>
 	)
