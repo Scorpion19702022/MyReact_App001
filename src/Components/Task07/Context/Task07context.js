2261d4772fe4a563c2e35164d78f653afb38208a
skorpion <jmosinski@o2.pl>
2024-03-27 21:42:30 +0100

    test

--- src/Components/Task07/Context/Task07context.js
+++ src/Components/Task07/Context/Task07context.js
@@ -1,6 +1,7 @@
 import { createContext } from 'react'
 
 import cafe from '../assets/coffee.jpg'
+import tea from '../assets/tea.jpg'
 
 const Task07context = createContext()
 
@@ -12,11 +13,15 @@ export const ActionCaffee = ({ children }) => {
 			price: 14,
 			img: cafe,
 		},
+		{
+			id: 1,
+			product: 'herbata',
+			price: 8,
+			img: tea,
+		},
 	]
 
-	console.log(cafeProducts)
-
-	return <Task07context.Provider value={{}}>{children}</Task07context.Provider>
+	return <Task07context.Provider value={{ cafeProducts }}>{children}</Task07context.Provider>
 }
 
 export default Task07context
--- src/Components/Task07/TaskCafe07.js
+++ src/Components/Task07/TaskCafe07.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './Task07cafe.module.css'
+import Task07context from './Context/Task07context'
 
 const TaskCafe07 = () => {
-	return (
-		<div className={styles.wrapper_cafe}>
-			<div></div>
+	const { cafeProducts } = useContext(Task07context)
+
+	console.log(cafeProducts)
+
+	const cafeMenu = cafeProducts.map(item => (
+		<div key={item.id} className={styles.menu_cafe}>
+			<h1 className={styles.menu_product}>{item.product}</h1>
+			<img className={styles.menu_img} src={item.img} alt='' />
+			<h3 className={styles.menu_price}>Cena: {item.price}</h3>
+			<button className={styles.menu_btn}>Zamów</button>
 		</div>
-	)
+	))
+
+	return <div className={styles.wrapper_cafe}>{cafeMenu}</div>
 }
 
 export default TaskCafe07
