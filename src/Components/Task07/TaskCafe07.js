e7355d7e36063069eae09a3beab99d66fcef70a9
skorpion <jmosinski@o2.pl>
2024-03-29 22:14:24 +0100

    test

--- src/Components/Task07/TaskCafe07.js
+++ src/Components/Task07/TaskCafe07.js
@@ -9,7 +9,7 @@ const TaskCafe07 = () => {
 
 	const cafeMenu = cafeProducts.map(item => (
 		<div key={item.id} className={styles.menu_cafe}>
-			<h1 className={styles.menu_product}>{item.product}</h1>
+			<h1 className={styles.menu_product}>{item.product.toUpperCase()}</h1>
 			<img className={styles.menu_img} src={item.img} alt='' />
 			<h3 className={styles.menu_price}>Cena: {item.price} zł</h3>
 			<div className={styles.box_btns}>
