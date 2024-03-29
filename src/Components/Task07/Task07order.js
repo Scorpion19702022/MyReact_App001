551459083cc57874b9b6ee6d0d40e71ee0c05d03
skorpion <jmosinski@o2.pl>
2024-03-29 21:51:18 +0100

    test

---
+++ src/Components/Task07/Task07order.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './Task07order.module.css'
+
+const Task07order = () => {
+	return (
+		<div className={styles.wrapper_order}>
+			<div>
+				<h1>Tekst</h1>
+			</div>
+		</div>
+	)
+}
+
+export default Task07order
