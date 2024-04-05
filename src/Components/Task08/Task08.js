dd9d2763f3fb0cdd84e988e7498722ce43e1b3fc
skorpion <jmosinski@o2.pl>
2024-04-05 20:39:08 +0200

    test

--- src/Components/Task08/Task08.js
+++ src/Components/Task08/Task08.js
@@ -7,7 +7,7 @@ const Task08 = () => {
 		<div className={styles.wrapper_task08}>
 			<div className={styles.header}>
 				<h1 className={styles.header_heading}>Task 08</h1>
-				<h4 className={styles.header_theme}>ToDo lista</h4>
+				<h4 className={styles.header_theme}>ToDo list</h4>
 			</div>
 		</div>
 	)
