10eee2add8ed08d515c5295796ae4b6d2961379b
skorpion <jmosinski@o2.pl>
2024-04-12 21:46:12 +0200

    test

--- src/Components/Task08/Context/Task08context.js
+++ src/Components/Task08/Context/Task08context.js
@@ -110,7 +110,7 @@ export const ToDoListCotext = ({ children }) => {
 	const handleTaskDone = id => {
 		const addToDone = taskDo.filter(item => item.id === id)
 		setTaskDoneList([...taskDoneList, ...addToDone])
-		if (!addToDone) {
+		if (addToDone) {
 			setTaskDo(prevState => [...prevState, ...addToDone])
 		}
 		// console.log(taskDoneList)
