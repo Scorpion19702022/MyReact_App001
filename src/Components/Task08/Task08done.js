d37f8543f5315971b4fae4533cea412b379433b9
skorpion <jmosinski@o2.pl>
2024-04-14 19:57:55 +0200

    test

--- src/Components/Task08/Task08done.js
+++ src/Components/Task08/Task08done.js
@@ -6,24 +6,23 @@ import Task08context from './Context/Task08context'
 const Task08done = () => {
 	const { taskDoneList, handleDeleteTaskDone } = useContext(Task08context)
 
-	const taskDone = taskDoneList.map(item => (
-		<div className={styles.task_done_lists} key={item.id}>
-			<div className={styles.tasks_done_important}>
-				<h4 className={item.important ? styles.heading_important : styles.heading_no_important}>{item.task}</h4>
+	const taskDone = taskDoneList
+		.map(item => (
+			<div className={styles.task_done_lists} key={item.id}>
+				<div className={styles.tasks_done_important}>
+					<h4 className={item.important ? styles.heading_important : styles.heading_no_important}>{item.task}</h4>
+				</div>
+				<div className={styles.task_done_dates}>
+					<em className={styles.task_done_date}>{item.taskDate}</em>
+				</div>
+				<div className={styles.task_done_btns}>
+					<button className={styles.btn_done} onClick={() => handleDeleteTaskDone(item.id)}>
+						usuń
+					</button>
+				</div>
 			</div>
-			<div className={styles.task_done_dates}>
-				<em className={styles.task_done_date}>{item.taskDate}</em>
-			</div>
-			<div className={styles.task_done_btns}>
-				<button className={styles.btn_done} onClick={() => handleDeleteTaskDone(item.id)}>
-					usuń
-				</button>
-			</div>
-		</div>
-	))
-
-	const test = taskDone
-	console.log(test)
+		))
+		.sort()
 
 	return (
 		<div className={styles.wrapper_task_done}>
