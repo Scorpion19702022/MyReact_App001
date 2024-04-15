d10840a728a9b4c344cb92850669ffd58f3a9b98
skorpion <jmosinski@o2.pl>
2024-04-15 21:20:30 +0200

    test

--- src/Components/Task09/Task09.js
+++ src/Components/Task09/Task09.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import styles from './Task09.module.css'
+import Task09one from './Task09one'
 
 const Task09 = () => {
 	return (
@@ -8,6 +9,9 @@ const Task09 = () => {
 			<div className={styles.box_header}>
 				<h1 className={styles.header_heading}>Task 09</h1>
 				<h3 className={styles.header_theme}>Zadania z inputami i tablicami</h3>
+			</div>
+			<div className={styles.wrapper}>
+				<Task09one />
 			</div>
 		</div>
 	)
---
+++ src/Components/Task09/Task09one.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Task09one.module.css'
+
+const Task09one = () => {
+	return (
+		<div className={styles.wrapper_task_one}>
+			<h1 className={styles.task_one_heading}>Zadanie pierwsze</h1>
+		</div>
+	)
+}
+
+export default Task09one
