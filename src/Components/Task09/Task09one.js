ccf83167643ac8abf04cd782d0d63145f67b675e
skorpion <jmosinski@o2.pl>
2024-04-16 21:07:15 +0200

    test

--- src/Components/Task09/Task09one.js
+++ src/Components/Task09/Task09one.js
@@ -6,6 +6,17 @@ const Task09one = () => {
 	return (
 		<div className={styles.wrapper_task_one}>
 			<h1 className={styles.task_one_heading}>Zadanie pierwsze</h1>
+			<div className={styles.box_inputs}>
+				<label className={styles.label}>Podaj imię</label>
+				<input className={styles.input} type='text' />
+				<label className={styles.label}>Podaj nazwisko</label>
+				<input className={styles.input} type='text' />
+				<label className={styles.label}>Podaj wiel</label>
+				<input className={styles.input} type='number' min={18} />
+				<div className={styles.btns_add}>
+					<button className={styles.btn_add}>Dodaj</button>
+				</div>
+			</div>
 		</div>
 	)
 }
