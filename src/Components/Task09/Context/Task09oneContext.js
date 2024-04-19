1a34467477de9aec1ab73495f58f88330754d853
skorpion <jmosinski@o2.pl>
2024-04-19 20:40:32 +0200

    test

--- src/Components/Task09/Context/Task09oneContext.js
+++ src/Components/Task09/Context/Task09oneContext.js
@@ -4,10 +4,34 @@ const Task09oneContext = createContext()
 
 export const Task09oneProvider = ({ children }) => {
 	const [firstName, setFirstName] = useState('')
-	const [lastName, setLastName] = useState()
+	const [lastName, setLastName] = useState('')
 	const [age, setAge] = useState(null)
 
-	return <Task09oneContext.Provider value={{ firstName, lastName, age }}>{children}</Task09oneContext.Provider>
+	// const [errorAge, setErrorAge] = useState(null)
+
+	const handleChangeFirstName = e => {
+		setFirstName(e.target.value)
+	}
+
+	const handleChangeLastName = e => {
+		setLastName(e.target.value)
+	}
+
+	const handleChangeAge = e => {
+		setAge(e.target.value)
+	}
+
+	console.log(firstName)
+	console.log(lastName)
+	console.log(age)
+
+	return (
+		<Task09oneContext.Provider
+			value={{ firstName, lastName, age, handleChangeFirstName, handleChangeLastName, handleChangeAge }}
+		>
+			{children}
+		</Task09oneContext.Provider>
+	)
 }
 
 export default Task09oneContext
--- src/Components/Task09/Task09one.js
+++ src/Components/Task09/Task09one.js
@@ -5,18 +5,19 @@ import Task09oneResult from './Task09oneResult'
 import Task09oneContext from './Context/Task09oneContext'
 
 const Task09one = () => {
-	const { firstName, lastName, age } = useContext(Task09oneContext)
+	const { firstName, lastName, age, handleChangeFirstName, handleChangeLastName, handleChangeAge } =
+		useContext(Task09oneContext)
 
 	return (
 		<div className={styles.wrapper_task_one}>
 			<h1 className={styles.task_one_heading}>Zadanie pierwsze</h1>
 			<div className={styles.box_inputs}>
 				<label className={styles.label}>Podaj imię</label>
-				<input className={styles.input} type='text' value={firstName} onChange={}/>
+				<input className={styles.input} type='text' value={firstName} onChange={handleChangeFirstName} />
 				<label className={styles.label}>Podaj nazwisko</label>
-				<input className={styles.input} type='text' value={lastName} onChange={}/>
+				<input className={styles.input} type='text' value={lastName} onChange={handleChangeLastName} />
 				<label className={styles.label}>Podaj wiel</label>
-				<input className={styles.input} type='number' min={18} value={age} onChange={}/>
+				<input className={styles.input} type='number' value={age} onChange={handleChangeAge} />
 				<div className={styles.btns_add}>
 					<button className={styles.btn_add}>Dodaj</button>
 				</div>
