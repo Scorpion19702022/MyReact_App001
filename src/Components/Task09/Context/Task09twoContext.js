8400ced96ab5081556c9fc02761a83769c560292
skorpion <jmosinski@o2.pl>
2024-04-22 19:47:35 +0200

    test

--- src/Components/Task09/Context/Task09twoContext.js
+++ src/Components/Task09/Context/Task09twoContext.js
@@ -1,5 +1,7 @@
 import React, { createContext, useState } from 'react'
 
+import { v4 as uuidv4 } from 'uuid'
+
 const Task09twoContext = createContext()
 
 export const Task09twoProvider = ({ children }) => {
@@ -7,6 +9,16 @@ export const Task09twoProvider = ({ children }) => {
 	const [carYear, setCarYear] = useState('')
 
 	const year = new Date().getFullYear()
+
+	const [carInfo, setCarInfo] = useState([])
+
+	const [carTable, setCarTable] = useState([
+		{
+			id: uuidv4(),
+			carName: '',
+			carAge: '',
+		},
+	])
 
 	return <Task09twoContext.Provider value={{ car, carYear }}>{children}</Task09twoContext.Provider>
 }
--- src/Components/Task09/Task09two.js
+++ src/Components/Task09/Task09two.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './Task09two.module.css'
 import Task09twoResult from './Task09twoResult'
+import Task09twoContext from './Context/Task09twoContext'
 
 const Task09two = () => {
+	const { car, yearCar } = useContext(Task09twoContext)
+
 	return (
 		<div className={styles.wrapper_task_two}>
 			<h1 className={styles.task_two_heading}>Zadanie drugie</h1>
 			<div className={styles.box_inputs}>
 				<label className={styles.label}>wpisz markę samochodu:</label>
-				<input className={styles.input} type='text' />
+				<input className={styles.input} type='text' value={car} />
 				<label className={styles.label}>Podaj rok produkcji:</label>
-				<input className={styles.input} type='number' />
+				<input className={styles.input} type='number' value={yearCar} />
 				<div className={styles.box_btns}>
 					<button className={styles.btn}>Dodaj</button>
 				</div>
