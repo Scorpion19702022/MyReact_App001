e2c032eb2c36b9faf37c984e9fa398da4e9db21b
skorpion <jmosinski@o2.pl>
2024-04-23 19:24:54 +0200

    test

--- src/Components/Task09/Task09twoResult.js
+++ src/Components/Task09/Task09twoResult.js
@@ -24,7 +24,7 @@ const Task09twoResult = () => {
 
 	return (
 		<div className={styles.wrapper_result}>
-			<h1 className={styles.heading_list}>Ilość samochodów:</h1>
+			<h1 className={styles.heading_list}>Ilość samochodów: {carInfo.length}</h1>
 			<div className={styles.box_info}>
 				<p className={styles.info}>INFORMACJA:</p>
 				<p className={styles.info}>{info}</p>
